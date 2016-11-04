7b76d83ca99e572f3733f15a0a053efe6e190eb8
Siddharth Atre <sidatre@gmail.com>
2016-11-04 14:17:20 -0700

    Significant refactor of class. Cleaned up code. New Asset folder structure.

--- Medical Imaging VR/Assets/LoadLegend.cs
+++ Medical Imaging VR/Assets/LoadLegend.cs
@@ -6,55 +6,72 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
+/**
+ * This script is responsible for loading the legend dataset. 
+ * It writes an ouput vol tex data file that can then be set in material.
+ */
 public class LoadLegend : MonoBehaviour
 {
 
-    //public Texture3D volumeData;
-    public List<Color> imageColors;
+    public int legendID = 1;
+    public int startIndex = 60;
+    public int endIndex = 1010;
+    public int indexIncrement = 5;
 
-    public static int startNumber = 60;
-    public static int finalLegendIndex = 1010;
+    private List<Color> imageColors;
+
     // Use this for initialization
     void Start()
     {
-        int numImages = 0;
-        int width = 0;
-        int height = 0;
+        imageColors = new List<Color>();
 
+        string folderPath = findLegendFolderWithID(legendID);
 
-        for (int i = startNumber; i <= finalLegendIndex; i+=5)
+        //Precalc the tex dimensions by loading the first image.
+        Texture2D first = Resources.Load(folderPath + startIndex.ToString().PadLeft(4, '0')) as Texture2D;
+        int width = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(first.width) / System.Math.Log(2)));
+        int height = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(first.height) / System.Math.Log(2)));
+        int numImages = (endIndex - startIndex + 1) / indexIncrement;
+
+        for (int i = startIndex; i <= endIndex; i += indexIncrement)
         {
-            Texture2D anImage = Resources.Load("Legend/" + i.ToString().PadLeft(4, '0')) as Texture2D;
-            width = anImage.width;
-            height = anImage.height;
+            Texture2D anImage = new Texture2D(width, height);
+            Texture2D temp = Resources.Load(folderPath + i.ToString().PadLeft(4, '0')) as Texture2D;
+            anImage.SetPixels(temp.GetPixels());
             addImageColorToList(anImage);
-            numImages++;
         }
-        Color[] allColors = imageColors.ToArray();
 
-        //Convert numImages to power of two
-        numImages = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(numImages) / System.Math.Log(2)));
-        width = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(width) / System.Math.Log(2)));
-        height = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(height) / System.Math.Log(2)));
-        int texSize = width * height * numImages;
+        //Convert volTex dimensions to nearest power of two
+        int numSlices = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(numImages) / System.Math.Log(2)));
 
-        Color[] allColorsWithPadding = new Color[texSize];
-
-        allColors.CopyTo(allColorsWithPadding, 0);
-        for (int i = allColors.Length; i < allColorsWithPadding.Length; i++)
+        //Need to pad the 3D tex along the z axis with empty data due to the round off.
+        Texture2D emptyTex = new Texture2D(width, height);
+        for(int j = numImages; j < numSlices; j++)
         {
-            allColorsWithPadding[i] = new Color(0, 0, 0, 0);
+            addImageColorToList(emptyTex);
         }
+        
+        //Allocate the texture memory.
+        Texture3D legendVolume = new Texture3D(width, height, numSlices, TextureFormat.ARGB32, false);
 
-        Texture3D volumeData = new Texture3D(width, height, numImages, TextureFormat.ARGB32, false);
-
-        volumeData.SetPixels(allColorsWithPadding);
-        volumeData.Apply();
+        //Copy the data to the 3D texture.
+        Color[] allColors = imageColors.ToArray();
+        legendVolume.SetPixels(allColors);
+        legendVolume.Apply();
 
         // assign it to the material of the parent object
-        GetComponent<Renderer>().material.SetTexture("Legend_Data", volumeData);
+        GetComponent<Renderer>().material.SetTexture("Legend_Data", legendVolume);
+
+        //As a sanity check, don't actually write out files if running from a compiled binary.
+#if UNITY_EDITOR
+        //Write out the loaded legend to an asset file for quick loading in the future.
+        AssetDatabase.CreateAsset(legendVolume, "Assets/Legend Assets/Legend_" + legendID.ToString() + ".asset");
+#endif
     }
 
+    /*
+     * Copy the pixel colors of the given image to the master array.
+     */
     void addImageColorToList(Texture2D anImage)
     {
         Color[] tempColors = anImage.GetPixels();
@@ -64,4 +81,21 @@ public class LoadLegend : MonoBehaviour
         }
     }
 
+    /**
+     * Function to find the corresponding legend data folder for the given legend ID
+     */
+    string findLegendFolderWithID( int id )
+    {
+        string dataPath = Application.dataPath;
+        string[] legendDirs = Directory.GetDirectories(dataPath + "/Resources/Legend/", "(" + id.ToString() + ")*");
+
+        //Assuming unique dir and system ids, get the first.
+        string folderPath = legendDirs[0];
+
+        //Extract just the last directory.
+        string[] folders = folderPath.Split('/');
+
+        //The last element should be the folder name.
+        return "Legend/" + folders[folders.Length - 1] + "/";
+    }
 }
