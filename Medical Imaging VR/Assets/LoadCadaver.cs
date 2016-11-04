67ee777b8de65e8c3f14957e9fa4857f9701850c
Siddharth Atre <sidatre@gmail.com>
2016-11-04 14:40:49 -0700

    Refactored LoadCadaver to new loading format specified by LoadLegend

--- Medical Imaging VR/Assets/LoadCadaver.cs
+++ Medical Imaging VR/Assets/LoadCadaver.cs
@@ -6,55 +6,62 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
+/**
+ * This script is responsible for loading the cadaver dataset specified by the legend being loaded. 
+ * It writes an ouput vol tex data file that can then be set in material.
+ */
 public class LoadCadaver : MonoBehaviour
-{
+{   
+    private List<Color> imageColors;
+    private static string folderPath = "Cadaver/";
 
-    //public Texture3D volumeData;
-    public List<Color> imageColors;
     // Use this for initialization
     void Start()
     {
+        //Get a ref to the legend loader script.
+        LoadLegend legendLoader = GetComponent<LoadLegend>();
+        imageColors = new List<Color>();
 
-        int numImages = 0;
-        int width = 0;
-        int height = 0;
+        int startIndex = legendLoader.startIndex;
+        int endIndex = legendLoader.endIndex;
+        int indexIncrement = legendLoader.indexIncrement;
+
+        //Precalc the tex dimensions by loading the first image.
+        Texture2D first = Resources.Load(folderPath + startIndex.ToString().PadLeft(4, '0')) as Texture2D;
+        int width = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(first.width) / System.Math.Log(2)));
+        int height = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(first.height) / System.Math.Log(2)));
+        int numImages = (endIndex - startIndex + 1) / indexIncrement;
 
         //need to correspond with legend's z-axis
-        for (int i = LoadLegend.startNumber; i <= LoadLegend.finalLegendIndex; i+=5)
+        for (int i = startIndex; i <= endIndex; i+=indexIncrement)
         {
-            Texture2D anImage = Resources.Load("Cadaver/"+i.ToString().PadLeft(4, '0')) as Texture2D;
-
-            width = anImage.width;
-            height = anImage.height;
+            Texture2D anImage = new Texture2D(width, height);
+            Texture2D temp = Resources.Load(folderPath + i.ToString().PadLeft(4, '0')) as Texture2D;
+            anImage.SetPixels(temp.GetPixels());
             addImageColorToList(anImage);
-            numImages++;
         }
+        int numSlices = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(numImages) / System.Math.Log(2)));
 
-        Color[] allColors = imageColors.ToArray();
+        //Allocate texture memory.
+        Texture3D volumeData = new Texture3D(width, height, numSlices, TextureFormat.ARGB32, false);
 
-        numImages = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(numImages) / System.Math.Log(2)));
-        width = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(width) / System.Math.Log(2)));
-        height = (int)System.Math.Pow(2, System.Math.Ceiling(System.Math.Log(height) / System.Math.Log(2)));
-        int texSize = width * height * numImages;
-
-        Color[] allColorsWithPadding = new Color[texSize];
-
-        allColors.CopyTo(allColorsWithPadding, 0);
-        for (int i = allColors.Length; i < allColorsWithPadding.Length; i++)
+        //Pad the color array with empty data to fill in for missing images.
+        Texture2D emptyTex = new Texture2D(width, height);
+        for(int j = numImages; j < numSlices; j++)
         {
-            allColorsWithPadding[i] = new Color(0, 0, 0, 0);
+            addImageColorToList(emptyTex);
         }
 
-        Texture3D volumeData = new Texture3D(width, height, numImages, TextureFormat.ARGB32, false);
-
-        volumeData.SetPixels(allColorsWithPadding);
+        //Copy data to texture memory.
+        Color[] allColors = imageColors.ToArray();
+        volumeData.SetPixels(allColors);
         volumeData.Apply();
 
         // assign it to the material of the parent object
         GetComponent<Renderer>().material.SetTexture("Cadaver_Data", volumeData);
         // save it as an asset for re-use
 #if UNITY_EDITOR
-        AssetDatabase.CreateAsset(volumeData, @"Assets/" + "Cadaver_400" + ".asset");
+        AssetDatabase.CreateAsset(volumeData, "Assets/Cadaver Assets/Cadaver_" + legendLoader.legendID.ToString() + ".asset");
 #endif
     }
 
