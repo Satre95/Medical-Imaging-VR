dabcca2aceed4b13f29f2b1c4a3624ce2ab7b613
kpyuan <kpyuan@ucsd.edu>
2016-11-29 18:50:18 -0800

    updated ui and connected with display body

---
+++ Medical Imaging VR/Assets/DisplayBody.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DisplayBody : MonoBehaviour {
+
+    public string DisplayId;
+    static DisplayBody displayBodyComp;
+
+	public void displayBodyParts(string idName)
+    {
+        //coming back from the display scene
+        if (displayBodyComp != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        displayBodyComp = this;
+        GameObject.DontDestroyOnLoad(this.gameObject);
+        DisplayId = idName;
+        SceneManager.LoadScene("Display Scene",LoadSceneMode.Single);
+    }
+}
---
+++ Medical Imaging VR/Assets/disableCanvas.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disableCanvas : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<Canvas>().enabled = false;
+    }
+}
